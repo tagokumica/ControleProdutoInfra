228349caa922fb30d77195cdc75e54db8dd5084f
tagokumica <49616408+tagokumica@users.noreply.github.com>
2025-05-04 15:54:54 -0300

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  cloud {
+    organization = "ControleProduto"
+    workspaces {
+      name = "ControleProdutoInfra"
+    }
+  }
+  required_provider{
+    aws = {
+      source = "hashcorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
