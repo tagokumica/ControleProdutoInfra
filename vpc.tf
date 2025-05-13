11aaca2db84f13ea7bcdb8d03ccb3432c4eb7ad0
tagokumica <tiago_tanp@hotmail.com>
2025-05-13 19:28:57 -0300

    add subnet public and private

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,26 @@ resource "aws_vpc" "main" {
     Name = "ControleProdutoVPC"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(local.cidr_block, 12, 1) # /28 (16 IPs totais, 8 utilizáveis)
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(local.cidr_block, 11, 1) # /27 (32 IPs totais, 16 utilizáveis)
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
