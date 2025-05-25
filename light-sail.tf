02094c9e50047c6319ab987cc401b007e6ff6f41
tagokumica <tiago_tanp@hotmail.com>
2025-05-25 17:56:35 -0300

    refact blueprint_id

--- light-sail.tf
+++ light-sail.tf
@@ -1,6 +1,6 @@
 resource "aws_lightsail_instance" "linux_instance" {
   name              = "controle-produto"
   availability_zone = "us-east-1a"
-  blueprint_id      = "ubuntu_24_04_lts"
+  blueprint_id      = "amazon_linux_2023"
   bundle_id         = "micro_2_0" 
 }
