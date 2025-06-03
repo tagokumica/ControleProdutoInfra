4cb4047087110c5bd15e1d15daa35598efc5a167
tagokumica <tiago_tanp@hotmail.com>
2025-06-03 20:46:10 -0300

    add iam for ecs

--- ecs.tf
+++ ecs.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_service" "fargate_service" {
-  name            = "fargate-service"
+  name            = "controle-produto-fargate-service"
   cluster         = aws_ecs_cluster.fargate_cluster.id
   task_definition = aws_ecs_task_definition.fargate_task.arn
   launch_type     = "FARGATE"
@@ -12,4 +12,27 @@ resource "aws_ecs_service" "fargate_service" {
   }
 
   depends_on = [aws_iam_role_policy_attachment.ecs_task_execution_role_policy]
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
