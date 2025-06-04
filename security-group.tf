5f06bd5f595095936440d753b7c50862312967ef
tagokumica <tiago_tanp@hotmail.com>
2025-06-03 21:48:36 -0300

    add ecs sg

--- security-group.tf
+++ security-group.tf
@@ -8,6 +8,26 @@ resource "aws_security_group" "allow_database" {
   }
 }
 
+resource "aws_security_group" "controle_produto_fargate_service" {
+  name        = "ecs-service-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_ipv4_database" {
   security_group_id = aws_security_group.allow_database.id
   cidr_ipv4         = aws_subnet.public.cidr_block
