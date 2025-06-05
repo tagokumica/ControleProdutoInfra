9efb7e0761146dc88aae132a277f1128f66d4396
tagokumica <tiago_tanp@hotmail.com>
2025-06-05 19:55:39 -0300

    update terraform ecs

--- ecs.tf
+++ ecs.tf
@@ -1,16 +1,5 @@
-resource "aws_ecs_service" "fargate_service" {
-  name            = "controle-produto-fargate-service"
-  cluster         = aws_ecs_cluster.fargate_cluster.id
-  launch_type     = "FARGATE"
-  desired_count   = 1
-
-  network_configuration {
-    subnets         = [aws_subnet.public.id]
-    assign_public_ip = true
-    security_groups  = [aws_security_group.allow_sg.id]
-  }
-
-  depends_on = [aws_iam_role_policy_attachment.ecs_task_execution_role_policy]
+resource "aws_ecs_cluster" "fargate_cluster" {
+  name = "controle_produto_fargate_cluster"
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
@@ -31,11 +20,47 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
+
+resource "aws_ecs_service" "fargate_service" {
+  name            = "controle-produto-fargate-service"
+  cluster         = aws_ecs_cluster.fargate_cluster.id
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = [aws_subnet.public.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.allow_sg.id]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.ecs_task_execution_role_policy]
+}
+
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_ecs_cluster" "fargate_cluster" {
-  name = "controle_produto_fargate_cluster"
+resource "aws_ecs_task_definition" "fargate_task" {
+  family                   = "fargate-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "controle-produto-app",
+      image     = "tagokumica/api-product:latest",
+      essential = true,
+      portMappings = [
+        {
+          containerPort = 8080,
+          hostPort      = 8080
+        }
+      ]
+    }
+  ])
 }
