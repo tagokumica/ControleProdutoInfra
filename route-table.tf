2f028be5d3b18ea69ac1d854b616e2cf62c02507
tagokumica <tiago_tanp@hotmail.com>
2025-06-13 21:04:23 -0300

    sync

--- route-table.tf
+++ route-table.tf
@@ -7,7 +7,7 @@ resource "aws_route_table" "public" {
   }
 
   tags = {
-    Name = "public-controle-produto"
+    Name = "route-controle-produto"
   }
 }
 
@@ -15,24 +15,4 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
-}
-
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "public-controle-produto"
-  }
-}
-
-# Associa rota pública à subnet pública
-resource "aws_route_table_association" "private_assoc" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
 }
