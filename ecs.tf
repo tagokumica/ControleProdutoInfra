d3b89d66246a381080eec9d42cdb86cd14f64fd2
tagokumica <tiago_tanp@hotmail.com>
2025-06-19 20:57:17 -0300

    rename sg in ecs

--- ecs.tf
+++ ecs.tf
@@ -31,7 +31,7 @@ resource "aws_ecs_service" "fargate_service" {
   network_configuration {
     subnets         = [aws_subnet.public.id]
     assign_public_ip = true
-    security_groups  = [aws_security_group.ecs_service.id]
+    security_groups  = [aws_security_group.ecs_sg.id]
   }
 
   depends_on = [aws_iam_role_policy_attachment.ecs_task_execution_role_policy]
